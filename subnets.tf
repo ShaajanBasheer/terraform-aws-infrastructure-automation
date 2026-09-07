9a9cb58a89ed081bceeca9a100729b2fad914e5d
ShaajanBasheer <112340032+ShaajanBasheer@users.noreply.github.com>
2026-09-07 19:51:48 +0530

    Update subnets.tf

---
+++ subnets.tf
@@ -0,0 +1,10 @@
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "terraform-demo-public-subnet"
+  }
+}
