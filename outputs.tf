9c184f21676eebec541058ac77b01a7825f6ebc3
ShaajanBasheer <112340032+ShaajanBasheer@users.noreply.github.com>
2026-09-07 19:53:25 +0530

    Update outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
