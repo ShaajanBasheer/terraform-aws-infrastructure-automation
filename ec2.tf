38ac8e49dac6bae5b349ba8e41f62a739e905232
ShaajanBasheer <112340032+ShaajanBasheer@users.noreply.github.com>
2026-09-07 19:52:59 +0530

    Update ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,32 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "terraform-demo-web-server"
+  }
+}
